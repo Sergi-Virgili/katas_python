37313737c86908f1ae7c8c2fd21cc786a6997b18
Sergi-Virgili <svirgilif@gmail.com>
2023-12-07 13:11:25 +0100

    Update FizzBuzz implementation

--- src/fizzbuzz/fizzbuzz.py
+++ src/fizzbuzz/fizzbuzz.py
@@ -1,7 +1,7 @@
 # Hacer una funcion que devuelva una lista de numeros de 1 al 100
-# Que si el numero es divisible por 3 -> Fizz
+# Que si el numero es divisible por 3 -> Fizzs
 # Que si el numero es divisible por 5 -> Buzz
-# Que si el numero es divisible por 3 y 5 -> FizzBuzz 
+# Que si el numero es divisible por 3 y 5 -> FizzBuzz
 # Que si no devuelve el numero [1,2,Fizz,4,Buzz,..]
 
-# USE TDD
+# USE TDD + Piensa en dividir el problema
